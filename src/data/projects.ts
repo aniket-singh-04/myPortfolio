2774240657d6dd8e98b01f9a3d12313c69d4321c
Aniket Singh <aniketsingh.lab@gmail.com>
2026-06-21 16:40:47 +0530

    add new projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -17,10 +17,31 @@ export const projects: Project[] = [
     title: 'Restaurant Food Ordering System',
     description:
       'QR-Based Restaurant Ordering System - A production-oriented MERN application that digitizes restaurant ordering through table QR codes, enabling seamless menu browsing, order placement, payment processing, and real-time order management.',
-    techStack: ['Node.js', 'Express.js', 'MongoDB', 'JWT Authentication', 'AWS Lambda', 'GitHub Actions', 'Razorpay', 'Nodemailer','Typescript', 'Redis', 'Git', 'GitHub', 'CI/CD'],
+    techStack: ['Node.js', 'Express.js', 'MongoDB', 'JWT Authentication', 'AWS Lambda', 'GitHub Actions', 'Razorpay', 'Nodemailer', 'Typescript', 'Redis', 'Git', 'GitHub', 'CI/CD'],
     githubLink: 'https://github.com/aniket-singh-04/myPortfolio',
     liveLink: 'https://mealtap.in',
     image: new URL("../assets/project2.png", import.meta.url).href,
     category: 'fullstack',
+  },
+  {
+    id: 3,
+    title: 'Redis Leaderboard API',
+    description:
+      'Redis Leaderboard API is a backend service built with Redis that provides efficient leaderboard management using sorted sets. It allows users to add scores, update rankings, and retrieve top players in real time, making it suitable for gaming, competitions, or ranking-based applications.',
+    techStack: ['Nodes', 'Redis', 'Express', 'Javascript'],
+    githubLink: 'https://github.com/aniket-singh-04/Redis_Leaderboard_API',
+    liveLink: '',
+    category: 'backend',
+  },
+  {
+    id: 4,
+    title: 'Simple Todo App',
+    description:
+      'A simple and clean Todo List web application that helps users add, manage, and track daily tasks efficiently. It features an easy-to-use interface where tasks can be added and marked as completed, helping improve productivity and organization.',
+    techStack: ['Html', 'CSS', 'Javascript'],
+    githubLink: 'https://github.com/aniket-singh-04/Todo-Web-App',
+    liveLink: 'https://simple-todos-web-app.netlify.app',
+    image: new URL("../assets/project0.png", import.meta.url).href,
+    category: 'frontend',
   },
 ];
